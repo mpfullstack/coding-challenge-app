61d9ee10fa99ce042510ac913133a6c741c19947
mpfullstack <info@marcperez.cat>
2021-02-13 09:32:41 +0100

    Render basic matches table data

--- src/modules/matches/components/MatchesTable.jsx
+++ src/modules/matches/components/MatchesTable.jsx
@@ -1,15 +1,29 @@
 import * as React from "react";
 import Table from "../../common/components/Table";
+import { getMatches } from "../../../services/matches";
+
+const columns = [
+  { field: 'id', headerName: 'Id', width: 120 },
+  { field: 'teams', headerName: 'Teams', width: 130 },
+  { field: 'date', headerName: 'Date', width: 130 },
+  { field: 'time', headerName: 'Time', width: 90 },
+  { field: 'result', headerName: 'Result', width: 160 }
+];
 
 const MatchesTable = () => {
   const [matchesData, setMatches] = React.useState([]);
 
-  React.useEffect(async () => {
-    const matches = await service.getMatches();
+  React.useEffect(() => {
+    // Using an IIFE
+    (async function() {
+      const matches = await getMatches();
+      setMatches(matches);
+    })();
+
   }, []);
 
   return (
-    <Table />
+    <Table rows={matchesData} cols={columns} />
   );
 }
 
