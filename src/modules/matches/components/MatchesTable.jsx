bfeda36606f75ff4bad4f9278f260d241cfd23d1
mpfullstack <info@marcperez.cat>
2021-02-14 18:29:49 +0100

    Match form component

--- src/modules/matches/components/MatchesTable.jsx
+++ src/modules/matches/components/MatchesTable.jsx
@@ -1,23 +1,11 @@
 import * as React from "react";
-import styled from "styled-components";
-import TextField from "../../common/components/TextField";
 import Table from "../../common/components/Table";
 import DeleteButton from "../../common/components/Buttons/DeleteButton";
 import Button from "../../common/components/Buttons/Button";
 import { getMatches, deleteMatch, createMatch } from "../../../services/matches";
 import { buildMatchColumns, Match } from "../../../models/match";
 import Drawer from "../../common/components/Drawer";
-import Form from "../../common/components/Form";
-
-const Fields = styled.div`
-  margin-top: 20px;
-  display: flex;
-  flex-direction: column;
-  width: 300px;
-  * > div {
-    margin-bottom: 20px;
-  }
-`;
+import MatchForm from "./MatchForm";
 
 const MatchesTable = () => {
   const [matchesData, setMatches] = React.useState([]);
@@ -34,6 +22,8 @@ const MatchesTable = () => {
   }, []);
 
   const columns = buildMatchColumns((params) => <DeleteButton onClick={async () => {
+    // NOTE: Delete confirmation might be added here
+
     // Simulate call to API to delete a match
     try {
       const id = await deleteMatch(params.getValue('id'));
@@ -48,45 +38,6 @@ const MatchesTable = () => {
     }
   }} />);
 
-  function renderForm(formState, input) {
-    // NOTE: There's no validation of data here, but might be added
-    return (
-      <Fields>
-        <TextField
-          label="Id"
-          id="id"
-          {...input.text({
-            name: "id"
-          })} />
-        <TextField
-          label="Teams"
-          id="teams"
-          {...input.text({
-            name: 'teams'
-          })} />
-        <TextField
-          label="Date"
-          id="date"
-          {...input.text({
-            name: 'date'
-          })} />
-        <TextField
-          label="Time"
-          id="time"
-          {...input.text({
-            name: 'time'
-          })} />
-        <TextField
-          label="Result"
-          id="result"
-          {...input.text({
-            name: 'result'
-          })} />
-        <Button type="submit">SAVE</Button>
-      </Fields>
-    )
-  }
-
   return (
     <div>
       <Table rows={matchesData} cols={columns} />
@@ -95,10 +46,7 @@ const MatchesTable = () => {
         visible={typeof match !== 'undefined'}
         onHide={() => addMatch(undefined)}
       >
-        <Form
-          formData={Match.shape()}
-          renderForm={renderForm}
-          onSubmit={async (e, values) => {
+        <MatchForm onSubmit={async (e, values) => {
             // Simulate call to API to create a match
             try {
               const match = await createMatch({ ...values })
